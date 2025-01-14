b49188ff9a010e587d19a0c0741031079b191bd2
Shubham Singh <shubhsoch@gmail.com>
2025-01-14 23:25:09 +0530

    copit edit

--- MathCamAI.py/copit.py
+++ MathCamAI.py/copit.py
@@ -45,15 +45,18 @@ def main():
     pTime = 0
     cap = cv2.VideoCapture(0)
     detector = handDetector()
+
     while True:
         success, img = cap.read()
         img = detector.findHands(img)
         lmList = detector.findPosition(img)
         if len(lmList) != 0:
             print(lmList[4])  # Print thumb tip position
+
         cTime = time.time()
         fps = 1 / (cTime - pTime)
         pTime = cTime
+        
         cv2.putText(img, f'FPS: {int(fps)}', (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
         cv2.imshow("Image", img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
